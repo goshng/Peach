c2525f868ff79b49c0a31162df013fb4ed343cc8
Sang Chul Choi <goshng@yahoo.co.kr>
2011-09-24 14:27:50 -0400

    The posterior probability of recombination was added to a UCSC genome browser.

--- ucscgb/files/dbDbInsert.sql
+++ ucscgb/files/dbDbInsert.sql
@@ -3,9 +3,30 @@ INSERT INTO dbDb
      defaultPos, active, orderKey, genome, scientificName,
      htmlPath, hgNearOk, hgPbOk, sourceName)
 VALUES
-    ("strPyg1", "Sep 19 2011", "/gbdb/strPyg1", "SpyogenesMGAS315",
-     "chr1:1-10000", 1, 1, "SpyogenesMGAS315", "Streptococcus mutans",
-     "/gbdb/strPyg1/html/description.html", 0, 0, "Streptococcus Recombination study version 1.0");
-INSERT INTO defaultDb (genome, name) VALUES ("SpyogenesMGAS315", "strPyg1");
-INSERT INTO genomeClade (genome, clade, priority) VALUES ("SpyogenesMGAS315", "streptococcus", 2);
+    ("Seeq4047", "Sep 23 2011", "/gbdb/Seeq4047", "S. equi subsp. equi 4047",
+     "chr1:1-100000", 1, 1, "S. equi subsp. equi 4047", "Streptococcus equi equi",
+     "/gbdb/Seeq4047/html/description.html", 0, 0, "Streptococcus recombination study version 1.0");
+INSERT INTO defaultDb (genome, name) VALUES ("S. equi subsp. equi 4047", "Seeq4047");
+INSERT INTO genomeClade (genome, clade, priority) VALUES ("S. equi subsp. equi 4047", "streptococcus", 9);
 /* INSERT INTO clade (name, label, priority) VALUES ("streptococcus", "Streptococcus", 45); */
+
+
+
+/* SdeqATCC12394 S. dys. equi. ATCC12394 
+   Streptococcus dysgalactiae equisimilis
+ */
+/* SdeqGGS124 S. dys. equi. GGS124
+   Streptococcus dysgalactiae equisimilis
+ */
+
+/* sdd? */
+
+/* SpyMGAS315 S. pyogenes MGAS315
+   Streptococcus pyogenes
+ */
+/* SpyMGAS10750 S. pyogenes MGAS10750
+   Streptococcus pyogenes
+ */
+/* Seeq4047 S. equi subsp. equi 4047
+   Streptococcus equi equi
+ */
--- ucscgb/run.sh
+++ ucscgb/run.sh
@@ -18,34 +18,88 @@ export SQL_PASSWORD=$USER
 
 #########################################################################
 # Let's add a new genome to streptoccocus genus.
-# Change the DBNAME.
-# strPyg1 is the genome for RNA-Seq data.
-DBNAME=strPyg1
-REFGENOMEFASTA=/Volumes/Elements/Documents/Projects/mauve/bacteria/Streptococcus_pyogenes_MGAS315_uid57911/NC_004070.fna
-cp $REFGENOMEFASTA .
-# Edit the genome FASTA-file so that the header is chr1.
-DBNAME=strPyg1
-REFGENOMEFASTA=NC_004070.fna
-hgFakeAgp -minContigGap=1 $REFGENOMEFASTA $DBNAME.agp
-faToTwoBit $REFGENOMEFASTA $DBNAME.2bit
+#
+# SdeqATCC12394
+# REFGENOMEFASTA=/Volumes/Elements/Documents/Projects/mauve/bacteria/cornell_sde1/CP002215.gbk
+# SdeqGGS124
+# REFGENOMEFASTA=/Volumes/Elements/Documents/Projects/mauve/bacteria/Streptococcus_dysgalactiae_equisimilis_GGS_124_uid59103/NC_012891.fna
+# SddyATCC27957
+# REFGENOMEFASTA=/Volumes/Elements/Documents/Projects/mauve/bacteria/cornell_sdd/sdd.gbk
+# SpyMGAS315
+# REFGENOMEFASTA=/Volumes/Elements/Documents/Projects/mauve/bacteria/Streptococcus_pyogenes_MGAS315_uid57911/NC_004070.fna
+# SpyMGAS10750
+# REFGENOMEFASTA=/Volumes/Elements/Documents/Projects/mauve/bacteria/Streptococcus_pyogenes_MGAS10750_uid58575/NC_008024.fna
+# Seeq4047
+# REFGENOMEFASTA=/Volumes/Elements/Documents/Projects/mauve/bacteria/Streptococcus_equi_4047_uid59259/NC_012471.fna
+# 
+# 1. Copy the source file: genbank or FASTA format.
+
+KENT=/Users/goshng/Documents/Projects/ucsc/kent
+KENTPRODUCT=/Users/goshng/Documents/Projects/ucsc/kent/src/product
+SQL_PASSWORD=$USER
+
+###########################################################
+# BEGIN of load bacterial genome function 
+###########################################################
+function loadBacterialGenome {
+echo -n "What is the database name for the genome? (e.g., SddyATCC27957) "
+read DBNAME
+REFGENOME=$DBNAME
+
+echo -n "Will you use a genbank format for creating $DBNAME? (Type in y or n) "
+read WISH
+if [ "$WISH" == "y" ]; then
+  echo -n "What is the file name of a genbank format? (e.g., /path/to/$DBNAME.gbk) "
+  read REFGENOMEFASTA
+  echo "  copying $REFGENOMEFASTA ..."
+  cp $REFGENOMEFASTA $DBNAME.gbk
+  echo "  converting the gbk to the FASTA file using Kent's gbToFaRa command ..."
+  gbToFaRa /dev/null $REFGENOME.fna $REFGENOME.ra $REFGENOME.ta $REFGENOME.gbk 
+  toUpper $REFGENOME.fna $REFGENOME.fna.upper
+  faSize $REFGENOME.fna.upper
+  rm $REFGENOME.fna $REFGENOME.ra $REFGENOME.ta 
+  mv $REFGENOME.fna.upper $REFGENOME.fna
+  echo -n "Please, Edit `pwd`/$REFGENOME.fna so that the header is chr1, and enter:"
+  read DONE
+fi
+
+echo -n "Will you use a FASTA format file for creating $DBNAME? (Type in y or n) "
+read WISH
+if [ "$WISH" == "y" ]; then
+  echo -n "What is the file name in FASTA format? (e.g., /path/to/$DBNAME.fna) "
+  read REFGENOMEFASTA
+  cp $REFGENOMEFASTA $DBNAME.fna
+  echo -n "Please, Edit `pwd`/$DBNAME.fna so that the header is chr1, and enter:"
+  read DONE
+fi
+
+echo "  creating a 2bit file of the FASTA file ..."
+hgFakeAgp -minContigGap=1 $REFGENOME.fna $DBNAME.agp
+faToTwoBit $REFGENOME.fna $DBNAME.2bit
 mkdir -p /gbdb/$DBNAME/html
 cp $DBNAME.2bit /gbdb/$DBNAME
+
+# 5. Check agp and 2bit.
 # sort -k1,1 -k2n,2n $DBNAME.agp > $DBNAME.agp.2
 # checkAgpAndFa $DBNAME.agp.2 $DBNAME.2bit
+echo "  creating a database ..."
 twoBitInfo $DBNAME.2bit stdout | sort -k2nr > chrom.sizes
 rm -rf bed
 mkdir -p bed/chromInfo
-awk '{printf "%s\t%d\t/gbdb/strMut1/strMut1.2bit\n", $1, $2}' \
-  chrom.sizes > bed/chromInfo/chromInfo.tab
+awk '{printf "%s\t%d\t/gbdb/DBNAME/DBNAME.2bit\n", $1, $2}' \
+  chrom.sizes > bed/chromInfo/chromInfo.tab.tmp
+sed s/DBNAME/$DBNAME/g < bed/chromInfo/chromInfo.tab.tmp > bed/chromInfo/chromInfo.tab
 hgsql -e "create database $DBNAME;" mysql
+
+echo "  creating grp, chromInfo tables ..."
 hgsql $DBNAME < $KENT/src/hg/lib/grp.sql
 cp bed/chromInfo/chromInfo.tab /tmp/
 hgLoadSqlTab $DBNAME chromInfo $KENT/src/hg/lib/chromInfo.sql \
   /tmp/chromInfo.tab
 rm /tmp/chromInfo.tab
 hgGoldGapGl $DBNAME $DBNAME.agp
-#
-DBNAME=strPyg1
+
+echo "  creating GC5 track ..."
 mkdir bed/gc5Base
 hgGcPercent -wigOut -doGaps -file=stdout -win=5 -verbose=0 $DBNAME \
   $DBNAME.2bit | wigEncode stdin bed/gc5Base/gc5Base.{wig,wib}
@@ -53,19 +107,23 @@ hgLoadWiggle -pathPrefix=/gbdb/$DBNAME/wib \
   $DBNAME gc5Base bed/gc5Base/gc5Base.wig
 mkdir -p /gbdb/$DBNAME/wib/bed/gc5Base
 cp bed/gc5Base/gc5Base.wib /gbdb/$DBNAME/wib/bed/gc5Base
-# Now, edit the database.
+
+echo -n "Please, edit files/dbDbInsert.sql for DB name, genome name, date, and scientific name and enter:"
+read DONE
 hgsql hgcentral < files/dbDbInsert.sql
-# Grant permissions.
+
+echo "  granting permission on the created database ..."
 mysql -u root -p${SQL_PASSWORD} -e "GRANT FILE ON *.* to browser@localhost \
 	IDENTIFIED BY 'genome';" mysql
-for DB in strPyg1 # hgcentral hg19 hg18 strMut1 hgFixed # proteins040315
+SQL_PASSWORD=$USER
+for DB in $DBNAME # hgcentral hg19 hg18 strMut1 hgFixed # proteins040315
 do
     mysql -u root -p${SQL_PASSWORD} -e "GRANT SELECT, INSERT, UPDATE, DELETE, \
 	CREATE, DROP, ALTER, CREATE TEMPORARY TABLES on ${DB}.* \
 	TO browser@localhost \
 	IDENTIFIED BY 'genome';" mysql
 done
-for DB in strPyg1 # hgcentral hg19 hg18 strMut1 hgFixed # cb1 proteins040315
+for DB in $DBNAME # hgcentral hg19 hg18 strMut1 hgFixed # cb1 proteins040315
 do
     mysql -u root -p${SQL_PASSWORD} -e "GRANT SELECT, CREATE TEMPORARY TABLES \
 	on ${DB}.* TO \
@@ -77,5 +135,48 @@ do
 	CREATE, DROP, ALTER on ${DB}.* TO readwrite@localhost \
 	IDENTIFIED BY 'update';" mysql
 done
-# Edit the local tracks
+echo "Please, edit the local tracks and make it"
+rm -f $DBNAME.fna $DBNAME.2bit $DBNAME.agp $DBNAME.gbk chrom.size
+rm -rf bed
+}
+###########################################################
+# END of load bacterial genome function 
+###########################################################
+
+###########################################################
+# Load wiggle files of recombination intensity.
+# TODO: the other 4 species should be added. The generation of the map for the
+# wiggle file takes time.
+function loadRI {
+  WIGDIR=/Users/goshng/Documents/Projects/Mauve/output/cornellf/3/run-analysis
+  WIGFILEBASE=ri1-refgenome4-map
+  DBNAME=SpyMGAS315
+  wigEncode $WIGDIR/$WIGFILEBASE.wig $WIGDIR/$WIGFILEBASE.temp.wig $WIGDIR/$WIGFILEBASE.wib
+  hgLoadWiggle $DBNAME ri $WIGDIR/$WIGFILEBASE.temp.wig
+  rm $WIGDIR/$WIGFILEBASE.temp.wig
+  mv $WIGDIR/$WIGFILEBASE.wib /gbdb/$DBNAME/wib/
+  hgsql $DBNAME -e "update ri set file='/gbdb/$DBNAME/wib/$WIGFILEBASE.wib'"
+}
+
+###########################################################
+# Load sequence alignments in wigMaf format.
+# Use /Users/goshng/Documents/Projects/Mauve/test/wigmaf/run
+# And make makeDb/trackDb file.
+# All of the mauve alignments were loaded.
+
+###########################################################
+# Load recombination probability in BED format.
+# Let me make something in /Users/goshng/Documents/Projects/Mauve/test/wigmaf/run
+
+###########################################################
+# Load virulence genes in BED format.
+
+
+###########################################################
+# Load recombination rate per block in BED format.
+#
+
+###########################################################
+# Load recombination probability.
+# 
 
