e912502e79337b7321aca9bba07e057ca91f3e68
Sang Chul Choi <goshng@yahoo.co.kr>
2011-12-15 08:53:42 -0500

    An update.

---
+++ biopp/run.sh
@@ -0,0 +1,15 @@
+
+git clone http://biopp.univ-montp2.fr/git/bpp-core.git
+git clone http://biopp.univ-montp2.fr/git/bpp-seq.git
+git clone http://biopp.univ-montp2.fr/git/bpp-phyl.git
+git clone http://biopp.univ-montp2.fr/git/bpp-popgen.git
+git clone http://biopp.univ-montp2.fr/git/bpp-raa.git
+git clone http://biopp.univ-montp2.fr/git/bpp-qt.git
+
+# for bpp-core
+# mkdir b; cd b
+# cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/usr
+#
+# for bpp-seq, and all others
+# mkdir b; cd b
+# cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/usr -DLINK_DIRECTORIES=$HOME/usr/lib -DINCLUDE_DIRECTORIES=$HOME/usr/include
